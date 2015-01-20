4252f7cb2f770cb827024b97c8bc0823dacda640
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-01-20 21:39:18 +0100

    add index for elements with support for json

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -7,6 +7,17 @@ class ElementsController < ApplicationController
       format.html
       format.json { render json: @element }
     end
-    
+
   end
+
+  def index
+    @elements = Element.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @elements }
+    end
+
+  end
+  
 end
