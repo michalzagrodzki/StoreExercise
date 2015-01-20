f4a3bd611220de37391fdacce3d7070ee3d9adf4
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-01-20 22:01:45 +0100

    correct route for /

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: "categories#index"
 
   #Custom root for all categories
-  get "/allcategories", to: "categories#index"
+  get "/allcategories", to: redirect("/")
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
