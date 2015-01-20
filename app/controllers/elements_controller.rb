2212f7a4e42b89019aec204e0d34ca235542b684
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-01-20 21:34:42 +0100

    add support for json

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -2,5 +2,11 @@ class ElementsController < ApplicationController
 
   def show
     @element = Element.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @element }
+    end
+    
   end
 end
